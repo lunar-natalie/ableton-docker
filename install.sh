ddece75a44b300dad0385a219a6ef7c7fe918619
Natalie Wiggins <islifepeachy@outlook.com>
2023-07-07 03:14:12 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 SRCDIR="$(realpath "$(dirname "$0")")"
 [[ -n "$NOENV" ]] || { source "$SRCDIR/env.sh" || exit 1; }
-curl -fsSL "https://raw.githubusercontent.com/mviereck/x11docker/master/x11docker" \
-    | sudo bash -s -- --update \
-    || exit 1
-mkdir -p "$HOME/.local/share/applications" && \
-    ln -sf "$SRCDIR/ableton-live.desktop" "$HOME/.local/share/applications/ableton-live.desktop"
-docker build -t ableton-docker . && \
-    "$SRCDIR/setup.sh" && \
-    exec "$SRCDIR/run.sh" wine64 explorer.exe
+if [[ -z "$NOUPDATE" ]]; then
+    curl -fsSL "https://raw.githubusercontent.com/mviereck/x11docker/master/x11docker" \
+        | sudo bash -s -- --update \
+        || exit 1
+    mkdir -p "$HOME/.local/share/applications" && \
+        ln -sf "$SRCDIR/ableton-live.desktop" "$HOME/.local/share/applications/ableton-live.desktop"
+    docker build -t ableton-docker . || exit 1
+fi
+"$SRCDIR/setup.sh" && \
+exec "$SRCDIR/run.sh" wine64 explorer.exe
